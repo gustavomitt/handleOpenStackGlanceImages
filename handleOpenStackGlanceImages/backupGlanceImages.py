b4095728c3c726742e1a3222790f8b6d200d7f67
Gustavo Mitt <gustavo.mitt@gmail.com>
2015-08-18 21:31:47 -0300

    Primeira versão

--- handleOpenStackGlanceImages/backupGlanceImages.py
+++ handleOpenStackGlanceImages/backupGlanceImages.py
@@ -7,29 +7,26 @@ Created on 18 de ago de 2015
 from keystoneclient.auth.identity import v2 as identity
 from keystoneclient import session
 from glanceclient import Client
+import os
 
-auth = identity.Password(auth_url=OS_AUTH_URL,
-                         username=OS_USERNAME,
-                         password=OS_PASSWORD,
-                         tenant_name=OS_TENANT_NAME)
+auth = identity.Password(auth_url=os.environ['OS_AUTH_URL'],
+                         username=os.environ['OS_USERNAME'],
+                         password=os.environ['OS_PASSWORD'],
+                         tenant_name=os.environ['OS_TENANT_NAME'])
 
 sess = session.Session(auth=auth)
 token = auth.get_token(sess)
 
 glance = Client('2',
-                endpoint=OS_GLANCE_ENDPOINT,
+                endpoint=os.environ['OS_GLANCE_ENDPOINT'],
                 token=token)
 
 for image in glance.images.list():
-    print image
+    filename = image.name + "." + image.disk_format
+    image_file = open(filename, 'w+')
+    for chunk in glance.images.data(image.id):
+        image_file.write(chunk)
+    print "Image wrote: " + filename
+    
+    
    
-cirros = glance.images.get("fd2c1699-fe0b-4c8d-8a4b-065831fe6360")
-
-d = glance.images.data(cirros.id)
-
-filename = cirros.name + "." + cirros.disk_format
-
-image_file = open(filename, 'w+')
-
-for chunk in glance.images.data(cirros.id):
-    image_file.write(chunk)
